1142e9c7adf405368fb2cfe9b0afa29d744ba30a
Pragati Malviya <malviyapragati43@gmail.com>
2023-11-27 16:33:28 +0530

    added validations & callbacks

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,19 @@
 class Course < ApplicationRecord
 	has_many :course_students
 	has_many :students, through: :course_students
+
+	validates :name, presence: true, length: { minimum: 5 }
+	validates :start_date, presence: true
+	validates :end_date, presence: true
+
+	before_validation do 
+		puts "Message before validations"
+	end
+	
+	before_save :course_save_message
+
+
+	def course_save_message
+		puts "This message is displayed before saving the course"
+	end
 end
